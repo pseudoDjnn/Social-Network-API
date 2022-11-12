299e04132b92e6efbbda7b59f2882500fe1de394
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-12 18:04:42 -0500

    start filling out models

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,19 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const ThoughtSchema = new Schema({});
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+  username: {
+    type: String,
+  },
+});
 
 const Thought = model("Thought", ThoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,25 @@
 const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
 
 const User = model("User", UserSchema);
 
