e0530ea120fe26e26c63f172dbc0d9f5efdaf782
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-13 18:58:36 -0500

    filled out models for routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
 const ThoughtSchema = new Schema(
@@ -30,12 +30,34 @@ const ThoughtSchema = new Schema(
   }
 );
 
-const ReactionSchema = new Schema({
-  reactionid: {},
-  reactionBody: {},
-  username: {},
-  createdAt: {},
-});
+const ReactionSchema = new Schema(
+  {
+    reactionid: {
+      type: Schema.Types.ObjectId,
+      default: () => newTypes.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLen: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // VIRTUAL COUNTER FOR REACTIONS
 ThoughtSchema.virtual("reactionCount").get(function () {
