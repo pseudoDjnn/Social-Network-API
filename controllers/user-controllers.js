b0d5c25d72197e3c20803be5632af384af3ada8a
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-14 13:07:15 -0500

    insomia linked to create user is working

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -4,7 +4,8 @@ const userControl = {
   // GRAB ALL USERS
   getAllUsers(req, res) {
     User.find({})
-      .then((allData) => res.json(allData))
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
@@ -15,13 +16,13 @@ const userControl = {
   getUserId({ params }, res) {
     User.findOne({})
       .then((userId) => {
-        if (!userId) {
+        if (!dbUserData) {
           res
             .status(404)
             .json({ message: "No User can be found with the id!" });
           return;
         }
-        res.json(userId);
+        res.json(dbUserData);
       })
       .catch((err) => {
         console.log(err);
@@ -32,9 +33,40 @@ const userControl = {
   // CREATE/POST NEW USER
   createUser({ body }, res) {
     User.create(body)
-      .then((newUser) => res.json(newUser))
+      .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(400).json(err));
   },
-};
 
+  // UPDATE EXISTING USER
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res
+            .status(404)
+            .json({ message: 'Cannot update a user that doesn"t exist' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // DELETE USER
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "There is no such user." });
+        }
+      })
+      .then(() => {
+        res.json({ message: "User and thoughts have been deleted!" });
+      })
+      .catch((err) => res.json(err));
+  },
+};
 module.exports = userControl;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,12 +4,14 @@ const {
   getAllUsers,
   getUserId,
   createUser,
+  updateUser,
+  deleteUser,
 } = require("../../controllers/user-controllers");
 
 // /api/users
 router.route("/").get(getAllUsers).post(createUser);
 
 // /api/users/:id
-router.route("/:id").get().put().delete();
+router.route("/:id").get(getUserId).put(updateUser).delete(deleteUser);
 
 module.exports = router;
